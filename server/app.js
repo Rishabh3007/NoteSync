ec60f94785e1db5d0abe4b6578fd1ffde0055a67
Rishabh Jain <jainrishabh3007@gmail.com>
2023-08-27 15:38:20 +0530

    deploying changes

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors({
     credentials: 'include',
-    origin: ['http://localhost:3000', 'https://notesync-frontend.onrender.com']
+    origin: ['http://localhost:3000', 'https://note-sync1.vercel.app']
 }));
 
 
