5966d1309d2e2fc69fbe171235bb03e834379d55
Rishabh Jain <jainrishabh3007@gmail.com>
2023-08-27 16:06:56 +0530

    deploying changes

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ app.use(cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }));
+  app.set('trust proxy', 1);
 
 
 dotenv.config({ path: '.env' }); //we can use this to set up environment variables in our application and then we can access them anywhere in our application
